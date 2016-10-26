7722e352231d6c4cd21c7a8ca3e74b33d1cf6dfe
App Academy Student <student@appacademy.io>
2016-10-26 14:40:44 -0700

    finished phase 4

--- frontend/components/root_component.jsx
+++ frontend/components/root_component.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, hashHistory } from 'react-router';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
+import { requestAllPokemon } from '../actions/pokemon_actions';
 
-const Root = ({store}) => (
-  <Provider store={store}>
+const Root = ({store}) => {
+  const requestOnEnter = () => {
+    store.dispatch(requestAllPokemon());
+  };
+
+  return (<Provider store={store}>
     <Router history={hashHistory}>
-      <Route path="/" component={PokemonIndexContainer } />
+      <Route path="/" component={PokemonIndexContainer} onEnter={requestOnEnter} />
     </Router>
-   </Provider>
-);
+  </Provider>);
+};
 
 export default Root;
